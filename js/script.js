672feb30dee5d4c7da69a850a1fe67454a7a38bd
Isis Lavor <isisnascimento142@gmail.com>
2025-06-08 21:26:09 -0300

    fix:adaptação do footer ao conteúdo

--- js/script.js
+++ js/script.js
@@ -1,10 +1,10 @@
 const nome = prompt("Digite seu nome:");
 const paragrafo = document.getElementById("mensagem-boas-vindas");
 if(nome != null){
-    paragrafo.innerHTML = `Olá  ${nome}! Tudo bem? Seja bem vindo(a)! \uD83E\uDD17\u2728<br><br>
+    paragrafo.innerHTML = `\uD83D\uDC4B Olá  ${nome}! Tudo bem? Seja bem vindo(a)! \uD83E\uDD17\u2728<br><br>
     Nós somos o MoodDaily, registre seu humor conosco! \u2764\uFE0F`;
 }
 else{
-    paragrafo.innerHTML = `Olá visitante! Tudo bem? Seja bem vindo(a)! \uD83E\uDD17\u2728<br><br>
+    paragrafo.innerHTML = `\uD83D\uDC4B Olá visitante! Tudo bem? Seja bem vindo(a)! \uD83E\uDD17\u2728<br><br>
     Nós somos o MoodDaily, registre seu humor conosco! \u2764\uFE0F`;
 };
